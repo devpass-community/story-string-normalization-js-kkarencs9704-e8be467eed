dff34e95cff2ca7819d4f5e1286cdc4d1fb0a68b
kkarencs <kcristinasouzaa1426@gmail.com>
2023-06-07 02:39:05 -0300

    Implementar método removeAccent

--- removeAccent.js
+++ removeAccent.js
@@ -1,5 +1,18 @@
 function removeAccent(str) {
-    // Add your solution here!
-}
-
-module.exports = removeAccent;
+    const accents = {
+      'a': /[àáâãäå]/g,
+      'e': /[èéêë]/g,
+      'i': /[ìíîï]/g,
+      'o': /[òóôõö]/g,
+      'u': /[ùúûü]/g,
+      'c': /[ç]/g,
+      'n': /[ñ]/g,
+    };
+  
+    for (let key in accents) {
+      str = str.replace(accents[key], key);
+    }
+  
+    return str;
+  }
+  
